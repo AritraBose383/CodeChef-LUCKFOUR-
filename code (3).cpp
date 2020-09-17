6f4f8240ac3de98a1d9de8bc56f1b7a6242fb541
Aritra Bose <64608228+AritraBose383@users.noreply.github.com>
2020-09-18 04:30:28 +0530

    Add files via upload

---
+++ code (3).cpp
@@ -0,0 +1,22 @@
+
+#include<iostream>
+using namespace std;
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		long long cnt=0;
+		string s;
+		cin>>s;
+		for(char c : s)
+		{
+			if(c=='4')
+			{
+				cnt++;
+			}
+		}
+		cout<<cnt<<endl;
+	}
+}
